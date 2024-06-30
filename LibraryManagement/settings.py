8366ed5c466281a04c13f022669cc1bcc3f6fde5
unknown <ijsathi2001rs@gmail.com>
2024-06-30 12:45:24 +0600

    allow

--- LibraryManagement/settings.py
+++ LibraryManagement/settings.py
@@ -16,8 +16,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS=['127.0.0.1', '0.0.0.0']
+CSRF_TRUSTED_ORIGINS = ['https://library-management-system-bzps.onrender.com/']
 
 # Application definition
 
