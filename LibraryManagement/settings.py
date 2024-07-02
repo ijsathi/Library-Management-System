7513ea250e7f7b63ca2a25e0b947d4d3261669af
unknown <ijsathi2001rs@gmail.com>
2024-07-02 12:36:25 +0600

    allow

--- LibraryManagement/settings.py
+++ LibraryManagement/settings.py
@@ -16,8 +16,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-# ALLOWED_HOSTS=['127.0.0.1', '0.0.0.0']
+# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS=['127.0.0.1', '0.0.0.0']
 CORS_ALLOWED_ORIGINS = [
     "https://library-management-system-bzps.onrender.com",
 ]
